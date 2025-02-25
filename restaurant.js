1c0b73ac36a03c694cc1e98f1e54e723a19d0a64
VCDhyan <dhyanvaradha2005@gmail.com>
2025-02-25 14:53:28 +0530

    res

---
+++ restaurant.js
@@ -0,0 +1,19 @@
+resurturant
+
+import mongoose from "mongoose";
+ const restaturantSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true } 
+ );
+
+const restaturantModel = mongoose.model("restaturant", restaturantSchema);
+export default restaturantModel;
